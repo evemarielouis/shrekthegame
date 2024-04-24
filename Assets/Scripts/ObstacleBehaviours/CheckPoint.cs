77d850780e0dba39a1bfcb35144ff9c27969fd6f
evemarielouis <148246045+evemarielouis@users.noreply.github.com>
2024-04-24 11:18:50 +0200

    dragon movement DONE

---
+++ Assets/Scripts/ObstacleBehaviours/CheckPoint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+
+    public delegate void TriggerEventHandler();
+    public static event TriggerEventHandler OnTriggerEvent;
+
+    public void OnTriggerEnter2D(Collider2D col) {
+		//Si l'obstacle entre en collision avec le joueur (objet avec le tag "Player")
+        if (col.gameObject.tag == "Player") {
+            
+			Debug.Log("Player entered the trigger collider!");
+            OnTriggerEvent?.Invoke();
+            
+			//On immobilise le joueur pendant 0.5 s
+			PlayerManager.SetFreeze(0.5f);
+        }
+    }
+}
+
