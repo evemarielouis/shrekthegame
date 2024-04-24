c3a803ced6e514a70f8d50223e7c5e8b956ea07d
evemarielouis <148246045+evemarielouis@users.noreply.github.com>
2024-04-24 12:25:55 +0200

    make sure trigger dragon only happens once

--- Assets/Scripts/ObstacleBehaviours/CheckPoint.cs
+++ Assets/Scripts/ObstacleBehaviours/CheckPoint.cs
@@ -7,17 +7,23 @@ public class CheckPoint : MonoBehaviour
 
     public delegate void TriggerEventHandler();
     public static event TriggerEventHandler OnTriggerEvent;
+    private bool StopTrigger = false;
 
     public void OnTriggerEnter2D(Collider2D col) {
 		//Si l'obstacle entre en collision avec le joueur (objet avec le tag "Player")
-        if (col.gameObject.tag == "Player") {
+        
+        if (!StopTrigger){
+            if (col.gameObject.tag == "Player") {
             
 			Debug.Log("Player entered the trigger collider!");
             OnTriggerEvent?.Invoke();
-            
+
 			//On immobilise le joueur pendant 0.5 s
 			PlayerManager.SetFreeze(0.5f);
+            StopTrigger = true;
         }
     }
+        }
+        
 }
 
